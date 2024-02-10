6e05b057614537fa3f77273c2d3d55dfd581029c
elaqaoui <mohamed.elaqaoui@gmail.com>
2024-02-10 23:59:02 +0100

    task upload

--- models/base_model.py
+++ models/base_model.py
@@ -15,14 +15,14 @@ class BaseModel:
             *args (any): Unused.
             **kwargs (dict): Key/value pairs of attributes.
         """
-        timeform = "%Y-%m-%dT%H:%M:%S.%f"
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
         if len(kwargs) != 0:
             for kv, val in kwargs.items():
                 if kv == "created_at" or kv == "updated_at":
-                    self.__dict__[kv] = datetime.strptime(val, timeform)
+                    self.__dict__[kv] = datetime.strptime(val, time_format)
                 else:
                     self.__dict__[kv] = val
         else:
@@ -39,13 +39,13 @@ class BaseModel:
         Includes the key/value pair __class__ representing
         the class name of the object.
         """
-        rdict = self.__dict__.copy()
-        rdict["created_at"] = self.created_at.isoformat()
-        rdict["updated_at"] = self.updated_at.isoformat()
-        rdict["__class__"] = self.__class__.__name__
-        return rdict
+        result_dict = self.__dict__.copy()
+        result_dict["created_at"] = self.created_at.isoformat()
+        result_dict["updated_at"] = self.updated_at.isoformat()
+        result_dict["__class__"] = self.__class__.__name__
+        return result_dict
 
     def __str__(self):
         """Return the print/str representation of the BaseModel instance."""
-        clname = self.__class__.__name__
-        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
