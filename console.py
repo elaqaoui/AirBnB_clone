28f8afd2889d274e69f6fe0e548002d195436107
elaqaoui <mohamed.elaqaoui@gmail.com>
2024-02-10 23:36:57 +0100

    task upload

--- console.py
+++ console.py
@@ -14,27 +14,27 @@ from models.review import Review
 
 
 def parse(arg):
-    curly_braces = re.search(r"\{(.*?)\}", arg)
+    curly_brackets = re.search(r"\{(.*?)\}", arg)
     brackets = re.search(r"\[(.*?)\]", arg)
-    if curly_braces is None:
+    if curly_brackets is None:
         if brackets is None:
             return [i.strip(",") for i in split(arg)]
         else:
-            lexer = split(arg[:brackets.span()[0]])
-            retl = [i.strip(",") for i in lexer]
-            retl.append(brackets.group())
-            return retl
+            token_parse = split(arg[:brackets.span()[0]])
+            return_list = [i.strip(",") for i in token_parse]
+            return_list.append(brackets.group())
+            return return_list
     else:
-        lexer = split(arg[:curly_braces.span()[0]])
-        retl = [i.strip(",") for i in lexer]
-        retl.append(curly_braces.group())
-        return retl
+        token_parse = split(arg[:curly_brackets.span()[0]])
+        return_list = [i.strip(",") for i in token_parse]
+        return_list.append(curly_brackets.group())
+        return return_list
 
 
 class HBNBCommand(cmd.Cmd):
-    """Defines the HolbertonBnB command interpreter.
-    Attributes:
-        prompt (str): The command prompt.
+    """Defines the alx command interpreter for the AirBnB console.
+    the Attributes:
+        prompt (str):
     """
 
     prompt = "(hbnb) "
@@ -49,27 +49,27 @@ class HBNBCommand(cmd.Cmd):
     }
 
     def emptyline(self):
-        """Do nothing upon receiving an empty line."""
+        """dont make anything when an empty line is entered."""
         pass
 
     def default(self, arg):
-        """Default behavior for cmd module when input is invalid"""
-        argdict = {
+        """Default behavior for cmd module."""
+        arguments_dict = {
             "all": self.do_all,
             "show": self.do_show,
             "destroy": self.do_destroy,
             "count": self.do_count,
             "update": self.do_update
         }
-        match = re.search(r"\.", arg)
-        if match is not None:
-            argl = [arg[:match.span()[0]], arg[match.span()[1]:]]
-            match = re.search(r"\((.*?)\)", argl[1])
-            if match is not None:
-                command = [argl[1][:match.span()[0]], match.group()[1:-1]]
-                if command[0] in argdict.keys():
-                    call = "{} {}".format(argl[0], command[1])
-                    return argdict[command[0]](call)
+        pattern_match = re.search(r"\.", arg)
+        if pattern_match is not None:
+            argl = [arg[:pattern_match.span()[0]], arg[pattern_match.span()[1]:]]
+            pattern_match = re.search(r"\((.*?)\)", argl[1])
+            if pattern_match is not None:
+                command = [argl[1][:pattern_match.span()[0]], pattern_match.group()[1:-1]]
+                if command[0] in arguments_dict.keys():
+                    function_call = "{} {}".format(argl[0], command[1])
+                    return arguments_dict[command[0]](function_call)
         print("*** Unknown syntax: {}".format(arg))
         return False
 
@@ -137,13 +137,13 @@ class HBNBCommand(cmd.Cmd):
         if len(argl) > 0 and argl[0] not in HBNBCommand.__classes:
             print("** class doesn't exist **")
         else:
-            objl = []
+            object_list = []
             for obj in storage.all().values():
                 if len(argl) > 0 and argl[0] == obj.__class__.__name__:
-                    objl.append(obj.__str__())
+                    object_list.append(obj.__str__())
                 elif len(argl) == 0:
-                    objl.append(obj.__str__())
-            print(objl)
+                    object_list.append(obj.__str__())
+            print(object_list)
 
     def do_count(self, arg):
         """Usage: count <class> or <class>.count()
